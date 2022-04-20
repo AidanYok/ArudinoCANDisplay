15f3b6f769c4ac72994f3a7d7cd85bd25ef7fba3
Controls Laptop <uwecocar3@gmail.com>
2022-04-19 18:19:53 -0700

    added tinter

--- display.py
+++ display.py
@@ -1,7 +1,18 @@
-import tkinter
+from tkinter import *
 
-root = tkinter.Tk()
+insert = 'peepee'
 
+root = Tk()
+
+# specify size of window.
+root.geometry("800x400")
+
+# Create label
+l = Label(root, text = insert)
+
+l.grid()
 
 root.mainloop()
+
+insert = 'poopoo'
 
--- main.py
+++ main.py
@@ -1,10 +1,23 @@
-import preProcessing
+#import preProcessing
+from tkinter import *
 
 msgs = ['43C', '038']
 refreshRate = 1
 
 
 loggedMsg = dict.fromkeys(msgs)
-while True:
-    preProcessing.preprocess(refreshRate, loggedMsg)
+
+root = Tk()
+def updateMsgs(refreshRate, loggedMsg):
+    #preProcessing.preprocess(refreshRate, loggedMsg)
     print(loggedMsg)
+    root.after(1000, updateMsgs)
+
+root.after(1000,updateMsgs(refreshRate, loggedMsg))
+l1 = Label(text = '0x43c: ' + str(loggedMsg['43C']))
+l2 = Label(text = '0x038: ' + str(loggedMsg['038']))
+
+l1.grid()
+l2.grid()
+
+root.mainloop()
